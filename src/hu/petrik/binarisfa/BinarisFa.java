7c10f00e30e7e49960539da2d4e55213732d6c9b
Morgován Nimród <morgovannimrod22@gmail.com>
2022-12-01 08:47:37 +0100

    getter, setter, BinarisFa osztály folytatása

--- src/hu/petrik/binarisfa/BinarisFa.java
+++ src/hu/petrik/binarisfa/BinarisFa.java
@@ -1,8 +1,21 @@
 package hu.petrik.binarisfa;
 
-public class BinarisFa<T> {
-    private BiFaElem fej;
-    private class BiFaElem<T> {
+public class BinarisFa<T extends Comparable<T>> {
+    private BiFaElem<T> fej;
+
+    public BinarisFa() {
+        this.fej = null;
+    }
+
+    public void beszur(T ertek) {
+        if (this.fej == null) {
+            this.fej = new BiFaElem<>(ertek);
+        } else {
+            fej.beszur(ertek)
+        }
+    }
+
+    private class BiFaElem<T extends Comparable<T>> {
         private T ertek;
         private BiFaElem<T> bal;
         private BiFaElem<T> jobb;
@@ -11,6 +24,38 @@ public class BinarisFa<T> {
             this.ertek = ertek;
             this.bal = null;
             this.jobb = null;
+        }
+
+        public T getErtek() {
+            return ertek;
+        }
+
+        public void setErtek(T ertek) {
+            this.ertek = ertek;
+        }
+
+        public BiFaElem<T> getBal() {
+            return bal;
+        }
+
+        public void setBal(BiFaElem<T> bal) {
+            this.bal = bal;
+        }
+
+        public BiFaElem<T> getJobb() {
+            return jobb;
+        }
+
+        public void setJobb(BiFaElem<T> jobb) {
+            this.jobb = jobb;
+        }
+
+        public void beszur(T ertek) {
+            if (ertek.compareTo(this.ertek) < 0) {
+                //TODO: beszúrás balra
+            } else {
+                //TODO: beszúrás jobbra
+            }
         }
     }
 }
